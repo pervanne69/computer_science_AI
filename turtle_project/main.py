1bf9a64d553bbf22e71181c12ea435feefdcd6eb
pervanne69 <samvel.pogosyan2006@gmail.com>
2023-12-03 18:39:52 +0300

    turtle_project_commit_2

--- turtle_project/main.py
+++ turtle_project/main.py
@@ -9,6 +9,8 @@ import random
 # end_fill() - конец заливки
 # color() - цвета
 
+turtle.speed(0)
+
 window = turtle.Screen()
 window.bgcolor('black')
 # Координаты окна
@@ -47,14 +49,14 @@ house_walls.speed(0)
 
 house_walls.color('red')
 house_walls.up()
-house_walls.setposition(-(n_x // 2 - 300), -(m_y // 4))
+house_walls.setposition(-(n_x // 2 - 350), -(m_y // 4))
 house_walls.down()
 
 
 def house_walls_fill():
     house_walls.begin_fill()
     for i in range(2):
-        house_walls.forward(n_x // 3)
+        house_walls.forward(n_x // 4)
         house_walls.left(90)
         house_walls.forward(m_y // 4)
         house_walls.left(90)
@@ -71,7 +73,7 @@ house_door.setposition(-((n_x // 2 - 300) // 3), -(m_y // 4))
 house_door.down()
 
 
-def house_door_dill():
+def house_door_fill():
     house_door.begin_fill()
     for i in range(2):
         house_door.forward(n_x // 15)
@@ -98,9 +100,71 @@ def house_door_dill():
     house_door.end_fill()
 
 
+house_door_fill()
+
+house_windows.speed(0)
+house_windows.color('black')
+house_windows.fillcolor('white')
+house_windows.up()
+hw_pos_1 = [-(n_x // 2 - 370), -(m_y // 9)]
+hw_pos_2 = [-(n_x // 2 - 540), -(m_y // 9)]
+house_windows.setposition(hw_pos_1[0], hw_pos_1[1])
 
 
-house_door_dill()
+def house_windows_fill():
+    house_windows.begin_fill()
+    for i in range(2):
+        house_windows.forward(40)
+        house_windows.left(90)
+        house_windows.forward(40)
+        house_windows.left(90)
+    house_windows.end_fill()
+    house_windows.down()
+    house_windows.color('black')
+    house_windows.forward(20)
+    house_windows.left(90)
+    house_windows.forward(40)
+    house_windows.right(90)
+    house_windows.forward(20)
+    house_windows.right(90)
+    house_windows.forward(20)
+    house_windows.right(90)
+    house_windows.forward(40)
+    house_windows.left(90)
+    house_windows.forward(20)
+    house_windows.left(90)
+    house_windows.forward(40)
+    house_windows.left(90)
+    house_windows.forward(40)
+    house_windows.left(90)
+    house_windows.forward(40)
+    house_windows.left(90)
+    house_windows.forward(20)
+
+
+house_windows_fill()
+house_windows.up()
+house_windows.setposition(hw_pos_2[0], hw_pos_2[1])
+house_windows.down()
+house_windows.left(90)
+house_windows.color('black')
+house_windows.fillcolor('white')
+house_windows_fill()
+house_windows.up()
+
+house_roof.color('purple')
+house_roof.begin_fill()
+house_roof.backward(150)
+h = 218
+a = (2 * h) / (3 ** 0.5)
+house_roof.speed(0)
+house_roof.left(60)
+house_roof.forward(a)
+house_roof.right(120)
+house_roof.forward(a)
+house_roof.right(120)
+house_roof.forward(a)
+house_roof.end_fill()
 
 
 # Функция по рисованию звезд
@@ -117,9 +181,24 @@ def star_fill(n, length):
 star_j.speed(0)
 star_j.color('yellow')
 count_of_stars = int(input("Введите количество звёздочек: "))
+
+
 for i in range(count_of_stars // 3):
     x = random.randint(-(n_x // 2 - 30), (n_x // 2 - 30))
-    y = random.randint(m_y // 2 - 100, m_y // 2)
+    if -195 < x < 195:
+        y = random.randint(100+round((200*100 - abs(x)*100)/200), round(m_y/2))+2
+    else:
+        y = random.randint(m_y // 2 - 100, m_y // 2)
+    star_j.up()
+    star_j.setposition(x, y)
+    star_j.down()
+    size = random.randint(5, 10)
+    star_fill(5, size)
+
+
+for i in range(count_of_stars // 3):
+    x = random.randint(-(n_x // 2), -(n_x // 2 - 330))
+    y = random.randint(-(m_y // 2 - 200), m_y // 2)
     star_j.up()
     star_j.setposition(x, y)
     star_j.down()
@@ -127,22 +206,20 @@ for i in range(count_of_stars // 3):
     star_fill(5, size)
 
 for i in range(count_of_stars // 3):
-    x = random.randint(-(n_x // 2 - 30), -(n_x // 2 - 200))
-    y = random.randint(-(m_y // 2 - 200), m_y // 2 - 100)
+    x = random.randint(n_x // 2 - 400, n_x // 2)
+    y = random.randint(-(m_y // 2 - 200), m_y // 2)
     star_j.up()
     star_j.setposition(x, y)
     star_j.down()
     size = random.randint(5, 10)
     star_fill(5, size)
 
-for i in range(count_of_stars // 3):
-    x = random.randint(n_x // 2 - 200, n_x // 2 - 30)
-    y = random.randint(-(m_y // 2 - 200), m_y // 2 - 100)
-    star_j.up()
-    star_j.setposition(x, y)
-    star_j.down()
-    size = random.randint(5, 10)
-    star_fill(5, size)
+house_windows.ht()
+house_walls.ht()
+house_door.ht()
+house_roof.ht()
+land.ht()
+star_j.ht()
 
 turtle.done()  # Рисунок закончен
 turtle.exitonclick()  # Выход при нажатии мышкой в окне Turtle
